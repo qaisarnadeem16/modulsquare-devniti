835faa8210b1601de03264f731dc795b8fd3de44
KhalidIrfani <khalidirfani4321@gmail.com>
2025-08-11 00:46:59 +0500

    font

--- src/components/common/custom-heading.tsx
+++ src/components/common/custom-heading.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Heading: React.FC<Props> = ({ children, className }) => {
   return (
     <h1
-      className={`md:text-[80px] font-alice outlined-heading  text-stroke text-stroke-primary  font-bold text-3xl md:leading-[73px] capitalize text-primary   ${className}`}
+      className={`md:text-[80px] font-poppins outlined-heading  text-stroke text-stroke-primary  font-bold text-3xl md:leading-[73px] capitalize text-primary   ${className}`}
     >
       {children}
     </h1>
